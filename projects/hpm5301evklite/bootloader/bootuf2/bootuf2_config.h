01cf2971080ac36165a60fb9b2f8f4695bbbbedf
HalfSweet <halfsweet@halfsweet.cn>
2024-08-08 17:03:07 +0800

    fix: uf2 config

--- projects/hpm5301evklite/bootloader/bootuf2/bootuf2_config.h
+++ projects/hpm5301evklite/bootloader/bootuf2/bootuf2_config.h
@@ -6,10 +6,10 @@
 #ifndef BOOTUF2_CONFIG_H
 #define BOOTUF2_CONFIG_H
 
-#define CONFIG_PRODUCT            "CheeryDAP"
+#define CONFIG_PRODUCT            "CherryDAP"
 #define CONFIG_BOARD              "HSLink Pro"
-#define CONFIG_BOOTUF2_INDEX_URL "https://github.com/cherry-embedded"
-#define CONFIG_BOOTUF2_JOIN_URL  "http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=GyH2M5XfWTHQzmZis4ClpgvfdObPrvtk&authKey=LmcLhfno%2BiW51wmgVC%2F8WoYwUXqiclzWDHMU1Jy1d6S8cECJ4Q7bfJ%2FTe67RLakI&noverify=0&group_code=642693751"
+#define CONFIG_BOOTUF2_INDEX_URL  "https://github.com/cherry-embedded/CherryDAP"
+#define CONFIG_BOOTUF2_JOIN_URL   "http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=NXGLdOOrFypXfBoAmh_eLwmcAZ2doqxJ&authKey=CDerxhytwBfYn1jHcfmBwOOl2B73vRxGmPJ0utVTFrnjfwfTikpzJbYUUPhyicmK&noverify=0&group_code=975779851"
 
 #define CONFIG_BOOTUF2_CACHE_SIZE         4096
 #define CONFIG_BOOTUF2_SECTOR_SIZE        512
