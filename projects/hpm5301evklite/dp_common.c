8ab24086915e057b5c515848e48aecc6db9fa5af
Runcheng Lu <runcheng.lu@hpmicro.com>
2024-09-15 21:32:21 +0800

    projects: hpm5301evklite: fixed some issue

--- projects/hpm5301evklite/dp_common.c
+++ projects/hpm5301evklite/dp_common.c
@@ -8,69 +8,74 @@
 #include "DAP.h"
 #include "hpm_spi_drv.h"
 
+#define SPI_MAX_SRC_CLOCK 80000000
+#define SPI_MID_SRC_CLOCK 60000000
+#define SPI_MIN_SRC_CLOCK 50000000
 void set_swj_clock_frequency(uint32_t clock)
 {
-    uint8_t div, sclk_div;
-    uint32_t div_remainder;
-    uint32_t div_integer;
-    uint32_t clk_src_freq_in_hz, sclk_freq_in_hz;
+    uint32_t div, sclk_div;
+    uint32_t sclk_freq_in_hz;
     sclk_freq_in_hz = clock;
     SPI_Type *spi_base = NULL;
     clock_name_t clock_name;
-    clk_src_freq_in_hz = clock_get_frequency(JTAG_SPI_BASE_CLOCK_NAME);
     clk_src_t src_clock = clk_src_pll1_clk0; /* 800M */
+    if (BOOST_KEIL_SWD_FREQ == 1) {
+        sclk_freq_in_hz *= 10;
+    }
+    PORT_Mode_t mode;
     if (DAP_Data.debug_port == DAP_PORT_SWD) {
-         if (clock >= 10000000) { /* clock >= 10M */
-            sclk_div = 0xFF; /* actual 80M*/
-            div = 10;
-        } else if ((clock >= 5000000) && (clock < 10000000)) { /* 5M <= clock < 10M */
-            sclk_div = 0xFF; /* actual 50M*/
-            div = 16;
-        } else if ((clock >= 2000000) && (clock < 5000000)) { /* 2M <= clock < 5M */
-            sclk_div = 1; /* actual 20M */
-            div = 10;
-        } else if ((clock >= 1000000) && (clock < 2000000)) { /* 1M <= clock < 2M */
-            sclk_div = 3; /* actual 10M */
-            div = 10;
-        } else if ((clock >= 500000) && (clock < 1000000)) { /* 500K <= clock < 1M */
-            sclk_div = 7; /* actual 5M */
-            div = 10;
-        } else if ((clock >= 200000) && (clock < 500000)) { /* 200K <= clock < 500K */
-            sclk_div = 19; /* actual 2M */
-            div = 10;
+        if (sclk_freq_in_hz < 100000) {
+            mode = PORT_MODE_GPIO;
         } else {
-            sclk_div = 39; /* actual 1M */
-            div = 10;
+            mode = PORT_MODE_SPI;
         }
+
+        // 判断是否需要切换模式
+        if (SWD_Port_Mode != mode) {
+            SWD_Port_Mode = mode;
+            PORT_SWD_SETUP();
+        }
+
         spi_base = SWD_SPI_BASE;
         clock_name = SWD_SPI_BASE_CLOCK_NAME;
     } else {
-        if (clock >= 40000000) {  /* >= 40M*/
-            sclk_div = 0xFF;
-            div = clock_get_frequency(clk_pll1clk0) / clock;
-        } else if ((clock >= 10000000) && (clock < 40000000)) { /* 10M <= clock < 40M */
-            clock_set_source_divider(JTAG_SPI_BASE_CLOCK_NAME, src_clock, 10);
-            clk_src_freq_in_hz = clock_get_frequency(JTAG_SPI_BASE_CLOCK_NAME);
-            div_remainder = (clk_src_freq_in_hz % sclk_freq_in_hz);
-            div_integer  = (clk_src_freq_in_hz / sclk_freq_in_hz);
-            if ((div_remainder != 0) || ((div_integer % 2) != 0)) {
-                sclk_div = 0;
-                clk_src_freq_in_hz = sclk_freq_in_hz * 2;
-                div = clock_get_frequency(clk_pll1clk0) / clk_src_freq_in_hz;
-            } else {
-                sclk_div = (div_integer / 2) - 1;
-                div = 10;
-            }
-        } else { /* < 10M */
-            sclk_div = 3;
-            clk_src_freq_in_hz = sclk_freq_in_hz * 8;
-            div = clock_get_frequency(clk_pll1clk0) / clk_src_freq_in_hz;
+        if (sclk_freq_in_hz < 100000) {
+            mode = PORT_MODE_GPIO;
+        } else {
+            mode = PORT_MODE_SPI;
         }
+
+        // 判断是否需要切换模式
+        if (JTAG_Port_Mode != mode) {
+            JTAG_Port_Mode = mode;
+            PORT_JTAG_SETUP();
+        }
+
         spi_base = JTAG_SPI_BASE;
         clock_name = JTAG_SPI_BASE_CLOCK_NAME;
     }
 
+    if (mode == PORT_MODE_GPIO) {
+        Set_Clock_Delay(sclk_freq_in_hz);
+        return;
+    }
+
+    sclk_div = ((SPI_MAX_SRC_CLOCK / sclk_freq_in_hz) / 2) - 1; /* SCLK = SPI_SRC_CLOK / ((SCLK_DIV + 1) * 2)*/
+    if (sclk_div <= 0xFE) {
+        div = 10;
+    } else {
+        div = 10;
+        src_clock = clk_src_pll0_clk1;                              /* 600M */
+        sclk_div = ((SPI_MID_SRC_CLOCK / sclk_freq_in_hz) / 2) - 1; /* SCLK = SPI_SRC_CLOK / ((SCLK_DIV + 1) * 2)*/
+        if (sclk_div >= 0xFE) {
+            div = 10;
+            src_clock = clk_src_pll1_clk2;  /* 500M */
+            sclk_div = ((SPI_MIN_SRC_CLOCK / sclk_freq_in_hz) / 2) - 1;
+            if (sclk_div >= 0xFE) {
+                sclk_div = 0xFE; /* The minimum sclk clock allowed is 98KHz */
+            }
+        }
+    }
     spi_master_set_sclk_div(spi_base, sclk_div);
     clock_set_source_divider(clock_name, src_clock, div);
 }
-
