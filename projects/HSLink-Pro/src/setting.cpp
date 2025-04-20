6e98096c7886c0b9941348ca79f5cbebf6c72d97
HalfSweet <halfsweet@halfsweet.cn>
2025-04-20 21:46:46 +0800

    fix

--- projects/HSLink-Pro/src/setting.cpp
+++ projects/HSLink-Pro/src/setting.cpp
@@ -191,7 +191,8 @@ static void parse_settings(std::string_view json)
     HSLink_Setting.led_brightness = root["led_brightness"].GetUint();
     HSLink_Setting.jtag_20pin_compatible = get_json_value(root, "jtag_20pin_compatible", false);
 
-    std::strcpy(HSLink_Setting.nickname, get_json_value(root, "nickname", ""));
+    std::strncpy(HSLink_Setting.nickname, get_json_value(root, "nickname", ""), sizeof(HSLink_Setting.nickname) - 1);
+    HSLink_Setting.nickname[sizeof(HSLink_Setting.nickname) - 1] = '\0';
 }
 
 static void load_settings()
